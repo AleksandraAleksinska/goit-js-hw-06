50ac73a061e7db5b6329c106cdafaaded131dd66
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-02-18 17:47:26 +0100

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+// console.log(ingredientsList);
+
+for (const ingredient of ingredients) {
+  const listElement = document.createElement('li');
+  listElement.textContent = ingredient;
+  listElement.classList.add("item");
+  ingredientsList.append(listElement);
+  
+}
+
+console.log(ingredientsList);
