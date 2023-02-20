4da44f19c8dcea7c68228379790555ade6e184eb
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-02-20 20:57:51 +0100

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const background = document.querySelector('body');
+// console.log(widget);
+const widgetColor = document.querySelector('.color')
+const button = document.querySelector('.change-color')
+
+const handleClick = (event) => {
+  widgetColor.textContent = getRandomHexColor();
+  background.style.backgroundColor = getRandomHexColor();
+  }
+
+button.addEventListener("click", handleClick);
