9f5f5de03fa189b2d258b8bbd56d58e3933d261d
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-02-20 20:12:34 +0100

    task 8

---
+++ src/js/task-08.js
@@ -0,0 +1,14 @@
+const loginForm = document.querySelector('.login-form');
+// console.log(loginForm);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {elements: {email, password}} = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return console.log(`Field cannot be empty!`);
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", handleSubmit);
