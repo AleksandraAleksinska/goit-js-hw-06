3773e3d47bd3537f8b73ac8aeb074bff7de57e0b
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-02-21 18:21:37 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener("input", (event) => {
-    if(!(nameInput.value === "")) {
+    if(nameInput.value !== "") {
     nameOutput.textContent = event.currentTarget.value;
     }
     else {
